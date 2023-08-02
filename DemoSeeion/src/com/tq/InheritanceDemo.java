2dffd7f2fc5dd9d44a9b933cfb9c24f85e2863e0
Jayshree-10 <tqjayshreec@gmail.com>
2023-08-02 14:08:11 +0530

    add statements

--- DemoSeeion/src/com/tq/InheritanceDemo.java
+++ DemoSeeion/src/com/tq/InheritanceDemo.java
@@ -5,6 +5,7 @@ public class InheritanceDemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		System.out.println("changes");
 	}
 
 }
